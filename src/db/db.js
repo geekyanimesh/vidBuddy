efec1589153dc255426dfc13d20778e7e7541e2d
geekyanimesh <animesh.kumar9651@gmail.com>
2025-08-08 23:58:58 +0530

    DB commit

--- src/db/db.js
+++ src/db/db.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants";
+import { DB_NAME } from "../constants.js";
 
 const connectDB = async ()=>{
     try {
       const connectionInstance = await mongoose.connect(`${process.env.MONGO_DB_URL}/${DB_NAME}`);
-      console.log(`DB connected ${connectionInstance}`);
+      console.log(`DB connected ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log("ERROR: ",error);
         throw error;  
     }
 }
-
-connectDB();
+export default connectDB;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-import mongoose, { mongo } from "mongoose";
-import { DB_NAME } from "./constants";
+import dotenv from 'dotenv'
+import connectDB from "./db/db.js";
 
-(async ()=>{
-    try {
-        await mongoose.connect(`${process.env.MONGO_DB_URL}/
-            ${DB_NAME}`)
-    } catch (error) {
-        console.log("ERROR: ",error);
-        throw err
-    }
-})() 
+dotenv.config({
+    path: './env'
+})
+
+connectDB()
 
