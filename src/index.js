42775a5719c55e8823ae69d75b96182decfbdfab
geekyanimesh <animesh.kumar9651@gmail.com>
2025-08-12 18:48:55 +0530

    api response beginning

--- src/index.js
+++ src/index.js
@@ -6,4 +6,11 @@ dotenv.config({
 })
 
 connectDB()
-
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server listening at ${process.env.PORT}`); 
+    })
+})
+.catch((err)=>{
+    console.log("Mongo connected",err);  
+})
